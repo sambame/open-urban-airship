24818cf4145775de01da0395b46ecdbb3dfa5b63
Shay Erlichmen <erlichmen@gmail.com>
2015-01-27 14:03:24 +0200

    fix error handling

--- validateP12.js
+++ validateP12.js
@@ -5,7 +5,7 @@
 var openssl = require('openssl-wrapper'),
     moment = require('moment');
 
-function verifyP12(signedData, password) {
+function validateP12(signedData, password) {
     return openssl.qExec('pkcs12', signedData, {password: "pass:"+password})
         .then(function(pemData) {
             return openssl.qExec('x509', pemData, {noout: true, enddate: true});
@@ -14,7 +14,7 @@ function verifyP12(signedData, password) {
             var prefix = "notAfter=";
             data = data.toString('utf8');
             if (data.lastIndexOf(prefix) !== 0) {
-                throw new Exception("Invalid file");
+                throw new Error("Invalid file mismatch header " + data);
             }
 
             data = data.substr(prefix.length);
@@ -22,11 +22,11 @@ function verifyP12(signedData, password) {
             var d = moment(data, 'ddd DD HH:mm:ss YYYY ZZ');
 
             if (!d.isValid()) {
-                throw new Exception("Invalid file");
+                throw new Error("Invalid file (date)");
             }
 
             if (d.toDate() < new Date()) {
-                throw new Exception("Certificate date has pass " + d.format());
+                throw new Error("Certificate date has pass " + d.format());
             }
 
             return d.toDate();
@@ -34,4 +34,4 @@ function verifyP12(signedData, password) {
 }
 
 
-module.exports = verifyP12;
+module.exports = validateP12;
