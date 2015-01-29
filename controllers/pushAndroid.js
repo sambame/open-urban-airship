9bcab1712013318a16b19562b54b4c1fb315644e
Shay Erlichmen <erlichmen@gmail.com>
2015-01-29 17:18:44 +0200

    add more logging for android failures

--- controllers/pushAndroid.js
+++ controllers/pushAndroid.js
@@ -6,15 +6,19 @@ var logger = require("../logger"),
     gcm = require("node-gcm"),
     buildMessage = require("./buildNotification");
 
+var retryCount = 3;
+
 var pushAndroidNotification = function(application, device, notification) {
     // create a message with default values
     var message = buildMessage(notification, "android", "data", gcm.Message),
         sender = new gcm.Sender(application.android.gcm_api_key),
         registrationIds = [device.token];
 
-    sender.send(message, registrationIds, 3, function (err, result) {
+    sender.send(message, registrationIds, retryCount, function (err, result) {
         if (err) {
-            logger.error(util.format("failed to send push %s", err));
+            logger.error(util.format("failed to send push: %s %s %s", device.token, device.alias, err));
+        } else if (result && result.failure) {
+            logger.error(util.format("failed to send push %s %s %s %s", device.token, device.alias, err, JSON.stringify(result)));
         } else {
             logger.info(util.format("finish sending %s", JSON.stringify(result)))
         }
