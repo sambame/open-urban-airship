64287d6284e33cfe8b2c1b7682f8ca274e70d6eb
Shay Erlichmen <erlichmen@gmail.com>
2015-02-14 21:11:49 +0200

    fix crash

--- controllers/pushAndroid.js
+++ controllers/pushAndroid.js
@@ -39,7 +39,7 @@ var pushAndroidNotification = function(application, device, notification) {
     sender.send(message, registrationIds, retryCount, function (err, result) {
         if (result && result.failure) {
             if (checkIfUninstall(result.results)) {
-                logger.info(util.format("%s device %s alias: %s has unregister, deactivation it", application.name, device.token, device.alias,));
+                logger.info(util.format("%s device %s alias: %s has unregister, deactivation it", application.name, device.token, device.alias));
                 DeviceModel.deactivateByToken(application, device.token);
             } else {
                 logger.error(util.format("%s failed to send push %s %s %s %s", application.name, device.token, device.alias, err, JSON.stringify(result)));
