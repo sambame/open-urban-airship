d5e9d617dd2504f433846cd06d373fde58843f8f
Shay Erlichmen <erlichmen@gmail.com>
2015-08-06 13:32:22 +0300

    add apid logging to android push

--- controllers/pushAndroid.js
+++ controllers/pushAndroid.js
@@ -65,7 +65,7 @@ var pushAndroidNotificationUsingSender = function(gcmSender, application, device
     gcmSender.send(message, registrationIds, retryCount, function (err, result) {
         if (result && result.failure) {
             if (checkIfUninstall(result.results)) {
-                logger.info(util.format("%s device %s alias: %s has unregister, deactivation it (thru gcm result)", application.name, device.token, device.alias));
+                logger.info(util.format("%s device %s alias: %s has unregister, deactivation it (thru gcm result) %s", application.name, device.token, device.alias), device.apid);
                 DeviceModel.deactivateByAPID(application, device.apid)
                     .then(function() {
                         if (callback) {
@@ -76,14 +76,14 @@ var pushAndroidNotificationUsingSender = function(gcmSender, application, device
                 return;
             }
 
-            logger.error(util.format("%s failed to send push %s %s %s %s", application.name, device.token, device.alias, err, JSON.stringify(result)));
+            logger.error(util.format("%s failed to send push %s %s %s %s %s", application.name, device.token, device.alias, err, JSON.stringify(result), device.apid));
         } else if (err) {
-            logger.error(util.format("%s failed to send push: %s %s %s", application.name, device.token, device.alias, err));
+            logger.error(util.format("%s failed to send push: %s %s %s %s", application.name, device.token, device.alias, device.apid, err));
         } else {
             var switchedToken = checkIfSwitchedToken(result.results);
 
             if (switchedToken) {
-                logger.info(util.format("%s: token switched %s => %s", application.name, device.token, switchedToken));
+                logger.info(util.format("%s: token switched %s => %s %s", application.name, device.token, switchedToken, device.apid));
                 DeviceModel.updateQ({_id: device._id}, {$set: {token: switchedToken}})
                     .then(function() {
                         // resend
@@ -94,7 +94,7 @@ var pushAndroidNotificationUsingSender = function(gcmSender, application, device
                         callback(err);
                     })
             } else {
-                logger.info(util.format("%s finish sending to token: %s alias: %s - %s", application.name, device.token, device.alias, JSON.stringify(result)));
+                logger.info(util.format("%s finish sending to token: %s alias: %s - %s %s", application.name, device.token, device.alias, JSON.stringify(result), device.apid));
             }
         }
 
